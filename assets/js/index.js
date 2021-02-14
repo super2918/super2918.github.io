e137a8f6854006395c8cf37c787eb0f1ec8c86e3
mc2918 <tlsdud1375@gmail.com>
2021-02-14 23:38:53 +0900

    Add home intro

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// const text = document.querySelector('.home__title');
+// let shadow = '';
+
+// const SHADOW_COUNT = 30;
+
+// for(let i = 0; i < SHADOW_COUNT; i++) {
+//   shadow += (shadow ? ',' : '') + -i * 1+'px ' + i * 1 + 'px #d9d9d9';
+// }
+
+// text.style.textShadow = shadow
+// console.log(text);
